48904d254ed3e0158ef155773e370cabfd89fed1
anderson <sp726129246@gmail.com>
2017-12-19 19:30:38 +0800

    Please enter the commit message for your changes. Lines starting

--- project_name/project_name.py
+++ project_name/project_name.py
@@ -5,8 +5,10 @@ from django.conf.urls import url
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
+
 def index(request):
     return HttpResponse('Hello world!')
+
 
 urlpatterns = (
     url(r'^$', index),
@@ -20,11 +22,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
-    DEBUG = DEBUG,
-    SECRET_KEY = SECRET_KEY,
-    ALLOWED_HOSTS = ALLOWED_HOSTS,
-    ROOT_URLCONF = __name__,
-    MIDDLEWARE_CLASSES = (
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',

dfa6d28b990bd064c380d797d1b8352f663fa7c5
anderson <sp726129246@gmail.com>
2017-12-21 13:57:06 +0800

    finish chapter3

--- sitebuilder/views.py
+++ sitebuilder/views.py
@@ -1,7 +1,9 @@
 import os
+import json
 from django.conf import settings
 from django.http import Http404
-from django.template import Template
+from django.template import Template, Context
+from django.template.loader_tags import BlockNode
 from django.utils._os import safe_join
 from django.shortcuts import render
 
@@ -17,6 +19,14 @@ def get_page_or_404(name):
 
     with open(file_path, 'r') as f:
         page = Template(f.read())
+
+    meta = None
+    for i, node in enumerate(list(page.nodelist)):
+        if isinstance(node, BlockNode) and node.name == 'context':
+            meta = page.nodelist.pop(i)
+            break
+    page._meta = meta
+
     return page
 
 
@@ -27,4 +37,9 @@ def page(request, slug='index'):
         'slug': slug,
         'page': page,
     }
+    if page._meta is not None:
+        meta = page._meta.render(Context())
+        extra_context = json.loads(meta)
+        context.update(extra_context)
+
     return render(request, 'page.html', context)

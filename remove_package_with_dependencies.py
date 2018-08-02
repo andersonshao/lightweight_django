172e0d348889634728177823aa0f5536459ec479
andersonshao <sp726129246@gmail.com>
2018-08-02 16:49:13 +0800

    添加一个卸载包极其依赖的程序

---
+++ remove_package_with_dependencies.py
@@ -0,0 +1,28 @@
+import os
+
+
+def check_dependencies(package_name):
+    with os.popen('pip show %s' % package_name) as p:
+        lines = p.readlines()
+        if not len(lines):
+            return None
+        dependencies = lines[-2].split(':')[-1].strip()
+        if dependencies:
+            dependencies = dependencies.split(',')
+        else:
+            dependencies = None
+        return dependencies
+
+
+def remove_package(package_name):
+    dependencies = check_dependencies(package_name)
+    if dependencies:
+        for dependency in dependencies:
+            remove_package(dependency)
+    os.system('pip uninstall -y %s' % package_name)
+
+
+
+if __name__ == '__main__':
+    package_name = input('请输入需要删除的包：')
+    remove_package(package_name)
